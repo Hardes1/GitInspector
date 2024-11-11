20dc298ae2969d690b42c013c8789f0d4dbf1576
Georgii Ustinov <georgii.ustinov@jebrains.com>
2024-11-11 16:02:04 +0200

    Rename GitMergeConflictSearcher

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ private val BASE_PATH = "${System.getProperty("user.home")}/Workspace/IdeaProjec
 
 private val GRADLE_PATH = "$BASE_PATH/gradle"
 private val SEMANTIC_MERGE_PATH = "$BASE_PATH/semantic-merge"
+private val FABRIC_PATH = "$BASE_PATH/fabric"
 
 fun main() {
     ConflictSearcher().search(SEMANTIC_MERGE_PATH)
