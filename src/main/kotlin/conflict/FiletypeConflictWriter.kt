73442a9b30806d03b12e735241bfe488d23452e1
Georgii Ustinov <ustinovgo@gmail.com>
2024-11-25 20:19:05 +0200

    Use ConcurrentHashMap instead of Mutex in FiletypeConflictWriter

--- src/main/kotlin/conflict/FiletypeConflictWriter.kt
+++ src/main/kotlin/conflict/FiletypeConflictWriter.kt
@@ -2,20 +2,19 @@ package conflict
 
 import data.RevisionInfo
 import data.WriterOptionContext
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import org.eclipse.jgit.revwalk.RevCommit
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.nio.file.Path
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import kotlin.io.path.Path
 import kotlin.io.path.extension
 import kotlin.io.path.pathString
 
 private val LOG = LoggerFactory.getLogger(FiletypeConflictWriter::class.java)
 class FiletypeConflictWriter(context: WriterOptionContext, repositoryPath: Path) : ConflictWriter(context, repositoryPath) {
-    private val mutex = Mutex()
-    private val filesMap: MutableMap<String, Int> = mutableMapOf()
+    private val filesMap: ConcurrentMap<String, Int> = ConcurrentHashMap()
 
     override suspend fun writeConflict(
         path: Path,
@@ -27,16 +26,16 @@ class FiletypeConflictWriter(context: WriterOptionContext, repositoryPath: Path)
             LOG.warn("Unable to get filetype for path: {}", path)
             return
         }
-        val number = mutex.withLock {
-            val number = filesMap.getOrDefault(extension, 0)
-            filesMap[extension] = number + 1
-            number
-        }
+        val number = getCurrentVersion(extension)
 
         for (revisionInfo in revisionInfoList) {
             val file = createDataFile(path, extension, number, revisionInfo.revisionType)
             file.writeText(revisionInfo.content)
         }
+    }
+
+    private fun getCurrentVersion(extension: String): Int {
+        return filesMap.compute(extension) { _, v -> (v ?: 0) + 1 } ?: 0
     }
 
     private fun createDataFile(path: Path, extensionName : String, number: Int, revisionType: RevisionType) : File {
