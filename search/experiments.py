2f642b09d8075df5277d272793c7b1db68d33c74
Georgii Ustinov <ustinovgo@gmail.com>
2025-05-28 15:24:49 +0300

    Add calculations for total evaluation

--- search/experiments.py
+++ search/experiments.py
@@ -1,7 +1,17 @@
 import statistics
-if __name__ == "__main__":
-    diff = statistics.read_data("semantic-merge.txt")
-    print('git conflict:', sum([i.before for i in diff]))
-    print('Semantic resolved conflict:', sum([i.after for i in diff]))
-    conflicts = [i for i in diff if i.after == 0 and i.before != 0]
+
+def diff_total():
+    diff =  statistics.read_data("semantic-diff.txt")
+    print('Regular highlighting:', sum([i.before for i in diff]))
+    print('Semantic diff highlighting:', sum([i.after for i in diff]))
+
+
+def merge_total():
+    merge = statistics.read_data("semantic-merge.txt")
+    print('Git conflict:', sum([i.before for i in merge]))
+    print('Semantic resolved conflict:', sum([i.after for i in merge]))
+    conflicts = [i for i in merge if i.after == 0 and i.before != 0]
     print(len(conflicts))
+
+if __name__ == "__main__":
+    diff_total()
